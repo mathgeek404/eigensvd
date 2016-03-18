227863aef8f295b7b0d947e2d8b2ecc2111f72b3
Steveoooo <mathgeek404@gmail.com>
2016-03-18 01:24:46 -0500

    more progres

--- src/eigenfaces.py
+++ src/eigenfaces.py
@@ -1,33 +1,52 @@
-import numpy
+import numpy as np
 from PIL import Image
 import os, re
 
 
+def pca(X) :
+	
+	
+	[n , d] = X.shape
 
-
+	#Remove mean
+	mu = X.mean(axis =0)
+	X = X-mu
+	
+	#Cov matrix
+	C = np.cov(X)
+    
+	[ eigenvalues , eigenvectors ] = np.linalg.eigh(C)
+		
+	idx = np.argsort(-eigenvalues)
+	eigenvalues = eigenvalues[ idx ]
+	eigenvectors = eigenvectors[:, idx]
+	
+	return [eigenvalues,eigenvectors,mu]
+	
 def main():
+    imgsize = 0
     datadir = "data/yalefaces/"
     imgList = os.listdir(datadir)
     numArr = []
     fileRegex = re.compile("subject.*")
     for fname in imgList:
         if (fileRegex.match(fname)):
-            img = Image.open(datadir+fname)
-            arr = numpy.array(img.getdata())
+            img = Image.open(datadir+fname).convert('L')
+            arr = np.array(img, 'uint8')
             numArr.append(arr)
-            
-    
- 
+            imgsize  = img.size
+         
     # Joing into matrix
     matrix = numArr[0]
     for i in range(1,len(numArr)-1):
-        matrix = numpy.vstack((matrix, numArr[i]))
+        matrix = np.vstack((matrix, numArr[i]))
     #matrix = numpy.transpose(matrix)
 
-    #Remove mean
-    mean = matrix.mean(axis=1)
-    matrix = matrix - mean[:, numpy.newaxis]
-    covar = numpy.cov(matrix)
+    [eigval, eigvec, mu] = pca(matrix)
+    
+    #for column in array.T:
+    #img = Image.fromarray(numArr[0])
+    img2.save('out.gif')
 
 
 
