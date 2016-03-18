2efd2ed0f353c00822d917295e2a712ce4ae30e5
Will Mitchell <mitchellmitchell@gmail.com>
2016-03-18 09:25:33 -0500

    Got svd version producing blurry faces, Friday 3.18

--- src/eigenfaces.py
+++ src/eigenfaces.py
@@ -1,11 +1,9 @@
 import numpy as np
 from PIL import Image
 import os, re
-
+from scipy.linalg import svd
 
 def pca(X) :
-	
-	
 	[n , d] = X.shape
 
 	#Remove mean
@@ -13,7 +11,7 @@ def pca(X) :
 	X = X-mu
 	
 	#Cov matrix
-	C = np.cov(X)
+	C = np.cov(X.T)
     
 	[ eigenvalues , eigenvectors ] = np.linalg.eigh(C)
 		
@@ -23,7 +21,8 @@ def pca(X) :
 	
 	return [eigenvalues,eigenvectors,mu]
 	
-def main():
+
+if 1:
     imgsize = 0
     datadir = "data/yalefaces/"
     imgList = os.listdir(datadir)
@@ -37,18 +36,27 @@ def main():
             imgsize  = img.size
          
     # Joing into matrix
-    matrix = numArr[0]
-    for i in range(1,len(numArr)-1):
-        matrix = np.vstack((matrix, numArr[i]))
-    #matrix = numpy.transpose(matrix)
+    matrix = numArr[0][...,np.newaxis]
+    for i in range(1,len(numArr)):
+        matrix = np.dstack((matrix, numArr[i][...,np.newaxis]))
+    #matrix = np.array(numArr).reshape(numArr[0].size,len(numArr))
+    g1 = matrix.reshape(243*320,165)
+    #matrix = np.array(numArr).reshape([77760,165])
+    u,s,v = svd(g1, full_matrices=0)
 
-    [eigval, eigvec, mu] = pca(matrix)
+    s[10:] = 0.0
+
+
+    # [eigval, eigvec, mu] = pca(matrix)
     
     #for column in array.T:
-    #img = Image.fromarray(numArr[0])
-    img2.save('out.gif')
+    for kk in range(165):
+        iv = np.dot(u,np.dot(np.diag(s),v[:,kk]))
+        #img = Image.fromarray(numArr[0])
+        img = Image.fromarray(iv.reshape(numArr[0].shape))
+        img.save('out%.4i.gif'%kk)
 
 
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
