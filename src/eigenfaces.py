b7e03cda29b74e66b9e6a5e33fb04d5e796d60c5
patron <patron@loan-sml-005-x.local>
2016-03-29 22:17:26 -0500

    code cleanup, added printout of eigenfaces

--- src/eigenfaces.py
+++ src/eigenfaces.py
@@ -1,62 +1,61 @@
+# Eigenface decomposition codes
+# By Sahit Mandala and Will Mitchell
+
 import numpy as np
 from PIL import Image
 import os, re
 from scipy.linalg import svd
 
-def pca(X) :
+def eigendec(X) :
 	[n , d] = X.shape
 
 	#Remove mean
 	mu = X.mean(axis =0)
 	X = X-mu
-	
+
 	#Cov matrix
 	C = np.cov(X.T)
-    
+
 	[ eigenvalues , eigenvectors ] = np.linalg.eigh(C)
-		
+
 	idx = np.argsort(-eigenvalues)
 	eigenvalues = eigenvalues[ idx ]
 	eigenvectors = eigenvectors[:, idx]
-	
+
 	return [eigenvalues,eigenvectors,mu]
-	
-
-if 1:
-    imgsize = 0
-    datadir = "data/yalefaces/"
-    imgList = os.listdir(datadir)
-    numArr = []
-    fileRegex = re.compile("subject.*")
-    for fname in imgList:
-        if (fileRegex.match(fname)):
-            img = Image.open(datadir+fname).convert('L')
-            arr = np.array(img, 'uint8')
-            numArr.append(arr)
-            imgsize  = img.size
-         
-    # Joing into matrix
-    matrix = numArr[0][...,np.newaxis]
-    for i in range(1,len(numArr)):
-        matrix = np.dstack((matrix, numArr[i][...,np.newaxis]))
-    #matrix = np.array(numArr).reshape(numArr[0].size,len(numArr))
-    g1 = matrix.reshape(243*320,165)
-    #matrix = np.array(numArr).reshape([77760,165])
-    u,s,v = svd(g1, full_matrices=0)
-
-    s[10:] = 0.0
 
 
-    # [eigval, eigvec, mu] = pca(matrix)
-    
-    #for column in array.T:
-    for kk in range(165):
-        iv = np.dot(u,np.dot(np.diag(s),v[:,kk]))
-        #img = Image.fromarray(numArr[0])
-        img = Image.fromarray(iv.reshape(numArr[0].shape))
-        img.save('out%.4i.gif'%kk)
+imgsize=(0,0)
+numimg = 0
+datadir = "data/yalefaces/"
+imgList = os.listdir(datadir)
+numArr = []
+fileRegex = re.compile("subject.*")
+for fname in imgList:
+	if (fileRegex.match(fname)):
+		img = Image.open(datadir+fname).convert('L')
+		arr = np.array(img, 'uint8').reshape(img.size[0]*img.size[1])
+		numArr.append(arr)
+		imgsize  = img.size
+		numimg += 1
 
+# Join into matrix, creating a (num of dim) x (num of data pts) matrix
+matrix = np.vstack(numArr).T
 
+#PCA/Eigendecomp
+u,s,v = svd(matrix, full_matrices=0)
 
-#if __name__ == '__main__':
-#    main()
+#Choose the k largest eigenvalues, vectors
+k=10
+s[k:] = 0.0
+
+#Eigenface construction
+for i in range(0,k):
+    img = Image.fromarray((u[:,i]*s[i]).reshape(imgsize[1],imgsize[0]))
+    img.save('result/eigenface%.4i.gif'%i)
+
+#Original image reconstruction
+for kk in range(0,numimg):
+    iv = np.dot(u,np.dot(np.diag(s),v[:,kk]))
+    img = Image.fromarray(iv.reshape(imgsize[1],imgsize[0]))
+    img.save('result/recon%.4i.gif'%kk)
